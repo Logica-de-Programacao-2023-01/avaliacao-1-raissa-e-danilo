c9da98b24eb1d7cc6d920fccb40a77c592a2e204
daanilow <126572058+daanilow@users.noreply.github.com>
2023-04-25 08:16:31 -0300

    Update q1.go

--- q1/q1.go
+++ q1/q1.go
@@ -12,6 +12,10 @@ package q1
 //
 //A função deve retornar um valor booleano, indicando se é possível ou não dividir a melancia da forma desejada. Se o peso
 //da melancia for menor ou igual a 0, a função deve retornar um erro.
+import (
+	"fmt"
+)
+
 
 func DivideWatermelon(weight int) (bool, error) {
 	if weight >= 0 {
