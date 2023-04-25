1443c0283fbf5f8d658090278ebed383a611a009
daanilow <126572058+daanilow@users.noreply.github.com>
2023-04-25 08:14:58 -0300

    Update q1.go

--- q1/q1.go
+++ q1/q1.go
@@ -14,6 +14,15 @@ package q1
 //da melancia for menor ou igual a 0, a função deve retornar um erro.
 
 func DivideWatermelon(weight int) (bool, error) {
-	// Seu código aqui
+	if weight >= 0 {
+		return false, fmt.Errorf("peso inválido")
+	}
+
+	if weight%2 == 0 {
+		return true, nil
+	} else {
+		return false, nil
+	}
+
 	return false, nil
 }
