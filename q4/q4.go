a7490f465e2772cf4a0a1a502c6837f2a3045f18
raissachpimenta <126472346+raissachpimenta@users.noreply.github.com>
2023-04-25 09:16:52 -0300

    Update q4.go

--- q4/q4.go
+++ q4/q4.go
@@ -15,7 +15,7 @@ import (
 
 func ClassifyPrices(prices []int) (int, error) {
 	if len(prices) == 0 {
-		return 0, fmt.Errorf("error")
+		return 0, fmt.Errorf("erro")
 	}
 	if len(prices) == 1 {
 		return 3, nil
