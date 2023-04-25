ab5e0de7c4f9ed6d8e8bb246429c9204ce22d9cb
daanilow <126572058+daanilow@users.noreply.github.com>
2023-04-25 08:20:54 -0300

    Update q1.go

--- q1/q1.go
+++ q1/q1.go
@@ -23,9 +23,9 @@ func DivideWatermelon(weight int) (bool, error) {
 	}
 
 	if weight%2 == 0 && weight > 2 {
-		return true, nil
-	} else {
 		return false, nil
+	} else {
+		return true, nil
 	}
 
 	return false, nil
