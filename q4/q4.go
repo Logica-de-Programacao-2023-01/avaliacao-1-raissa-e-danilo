7bba495d3cd48a88d37327ecb40ff0a55ee36454
raissachpimenta <126472346+raissachpimenta@users.noreply.github.com>
2023-04-25 09:09:32 -0300

    Update q4.go

--- q4/q4.go
+++ q4/q4.go
@@ -9,9 +9,7 @@ package q4
 //estiver em ordem decrescente e 3 se a lista estiver aleatória. A função deve retornar um erro se a lista estiver vazia.
 //Caso a lista possua apenas um elemento, a função deve retornar 3.
 
-import (
-	"fmt"
-)
+import "fmt"
 
 
 func ClassifyPrices(prices []int) (int, error) {
