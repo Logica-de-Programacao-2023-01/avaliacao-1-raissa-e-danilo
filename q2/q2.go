035ce7200c00fc803aa0d1b2f3fc3fcdb038338d
raissachpimenta <126472346+raissachpimenta@users.noreply.github.com>
2023-04-25 08:25:23 -0300

    Update q2.go

--- q2/q2.go
+++ q2/q2.go
@@ -13,6 +13,17 @@ package q2
 //Ajude os amigos a encontrar o número de problemas para os quais eles escreverão uma solução.
 
 func ProblemsSolved(answers [][3]bool) int {
-	// Seu código aqui
-	return 0
+	numProblem := 0
+	for _, row := range answers {
+		numTrue := 0
+		for _, opinion := range row {
+			if opinion == true {
+				numTrue++
+			}
+		}
+		if numTrue >= 2 {
+			numProblem++
+		}
+	}
+	return numProblem
 }
